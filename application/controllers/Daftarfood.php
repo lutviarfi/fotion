32d9f33bc46e63d19110e62614390bdaf196a821
aldisaf <aldis289@gmail.com>
2018-04-18 18:49:52 +0700

    solve problem

--- application/controllers/Daftarfood.php
+++ application/controllers/Daftarfood.php
@@ -3,7 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Daftarfood extends CI_Controller {
 
+
 	public function index()
 	{
 	 $this->load->view('listfood');
 		}
+}
