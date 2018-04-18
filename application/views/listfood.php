4044acc29846c18987239e344ca9eaf1a1bc7688
ansmuf <mufidahanisa@gmail.com>
2018-04-19 00:19:43 +0700

    add title on header "Food Donation"

--- application/views/listfood.php
+++ application/views/listfood.php
@@ -34,36 +34,10 @@
 <!--end intro-->
 <div class="container">
   <div class="row">
-    <div class="holder_content1">
-      <section class="group4">
-        <h3>Latest news</h3>
-        <article>
-          <h4>10.12.2011 - New cakes <span>Cras quis libero, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut </span></h4>
-        </article>
-      </section>
-    </div>
-    <!--end holder-->
-  </div>
-</div>
-<div class="container">
-  <div class="row">
     <div class="twelevecol">
       <section class="group1">
-        <h5>About us</h5>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. Vestibulum condimentum facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
+        <h5>MENU MAKANAN</h5>
         <a class="photo_hover3" href="#"><img src="<?php blink('patisserie/images/picture1.jpg') ?>" width="215" height="137" alt=""></a> <a href="#"><span class="button">Read more</span></a> </section>
-    </div>
-    <div class="fourcol">
-      <section class="group2">
-        <h5>Products</h5>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. Vestibulum condimentum facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero.</p>
-        <a class="photo_hover3" href="#"><img src="images/picture2.jpg" width="215" height="137" alt=""></a> <a href="#"><span class="button">Read more</span></a> </section>
-    </div>
-    <div class="fourcol last">
-      <section class="group3">
-        <h5>Gallery</h5>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec molestie. Sed aliquam sem ut arcu. Phasellus sollicitudin. Vestibulum condimentum facilisis nulla. In hac habitasse platea dictumst. Nulla nonummy. Cras quis libero. </p>
-        <a class="photo_hover3" href="#"><img src="images/picture3.jpg" width="215" height="137" alt=""></a> <a href="#"><span class="button">Read more</span></a> </section>
     </div>
   </div>
 </div>
@@ -72,10 +46,7 @@
   <div class="container copyright">
     <div class="row">
       <div class="sixcol">
-        <div id="FooterTree"> &copy; 2012 chez Lulu </div>
-      </div>
-      <div class="sixcol last">
-        <div id="FooterTwo">Website Template By <a target="_blank" href="http://www.marijazaric.com">marija zaric</a></div>
+        <div id="FooterTree"> &copy; 2018 Fotion Team</div>
       </div>
     </div>
   </div>
