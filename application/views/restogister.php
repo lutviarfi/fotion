1b036a3137d213098b2275c16e94de7ce25a63cd
iqrahabibi <37201936+iqrahabibi@users.noreply.github.com>
2018-04-19 02:57:26 -0700

    memnbuat form login yang terdiri dari text username dan text password

--- application/views/restogister.php
+++ application/views/restogister.php
@@ -56,7 +56,7 @@
 			<input type="submit" value="Login">
 		</form>
 	</div>
-	<p class="footer-agile">UBL © 2018</a>
+	<p class="footer-agile">FOTION © 2018</a>
 	</p>
     <script src="<?php blink('assets/global/plugins/jquery.min.js')?>" type="text/javascript"></script>
 <script src="<?php blink('assets/global/plugins/jquery-migrate.min.js')?>" type="text/javascript"></script>
