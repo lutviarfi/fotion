160dca8a9cb606b3e1831371a3e0228fe6c0cef8
andyndy14 <andychahyo10@gmail.com>
2018-04-19 05:38:02 -0700

    menambahkan fungsi insert pada controller regispicker

--- application/controllers/regispicker.php
+++ application/controllers/regispicker.php
@@ -15,6 +15,34 @@ class regispicker extends CI_Controller {
 		$this->load->view('regispicker');
     }
     
-    
+   //tambah data buku
+	public function InsertPicker(){
+
+		$nama = $this->input->post('nama');
+		$email = $this->input->post('email');
+		$lokasi = $this->input->post('lokasi');
+		$kondisi = $this->input->post('kondisi');
+		$password = $this->input->post('password');
+
+		$data = array(
+		'nama' =>$nama,
+		'email'=> $email,
+		'lokasi'=>$lokasi,
+		'kondisi' =>$kondisi,
+		'password' => md5($password)
+		);
+
+		$result = $this->CRUDModel->InsertPicker($data);
+		$data = NULL;
+		if($result){
+
+			$data['result'] = "Sukses";
+		}else{
+
+			$data['result'] = "Gagal";
+		}
+
+		redirect('CRUD');
+	} 
 	
 }
