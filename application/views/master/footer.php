0aae8e33dd428a063ccc0edf8e653c283e8e7660
ansmuf <mufidahanisa@gmail.com>
2018-04-24 16:56:28 +0700

    editing footer

--- application/views/master/footer.php
+++ application/views/master/footer.php
@@ -6,18 +6,14 @@
 					<ul class=" in">
 						<li><a href="#">Home</a></li>
 						<li><a href="#">Picker Registration</a></li>
-						<li><a href="#"></a></li>
-						<li><a href="#"></a></li>
-						<li><a href="contact.html">Contact</a></li>
+						<li><a href="regis.html">Restaurant Register</a></li>
 						<li><a href="login_3.html">Login</a></li>
-						<li><a href="regis.html">Register</a></li>
+
 					</ul>
-						<span>There are many variations of passages</span>
 				</div>
 				<div class="col-md-4 footer-bottom  animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
 					<h2>Follow Us</h2>
 					<label><i class="glyphicon glyphicon-menu-up"></i></label>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.</p>
 					<ul class="social-ic">
 						<li><a href="#"><i></i></a></li>
 						<li><a href="#"><i class="ic"></i></a></li>
@@ -30,7 +26,7 @@
 			<div class="clearfix"> </div>
 
 			</div>
-			<p class="footer-class animated wow bounce" data-wow-duration="1000ms" data-wow-delay="500ms">&copy; 2016 Cookery . All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+			<p class="footer-class animated wow bounce" data-wow-duration="1000ms" data-wow-delay="500ms">&copy; 2018 Fotion . All Rights Reserved | Design by Team Fotion </p>
 		</div>
 	</div>
 	<!--//footer-->
