1940d5d8f8f3c4058cb8e21159151ac00b2fb740
fadilahasana <fadilahasana97@gmail.com>
2018-04-25 12:19:27 +0700

    edit controller logic, adapt with database

--- application/controllers/InsertMakananController.php
+++ application/controllers/InsertMakananController.php
@@ -3,9 +3,49 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class InsertMakananController extends CI_Controller {
 
+  public function __construct() {
+		parent::__construct();
+		$this->load->model('ModelInsertMakanan');
+
+	}
 
 	public function index()
 	{
 	 $this->load->view('InsertMakanan');
-		}
+	}
+
+  public function InsetData(){
+  		$kode = $this->input->post('idmakanan');
+  		$nama = $this->input->post('nama');
+  		$expire = $this->input->post('expire');
+  		$halal = $this->input->post('halal');
+  		$available = $this->input->post('available');
+  		$ingredients = $this->input->post('ingredients');
+      $lokasi = $this->input->post('lokasi');
+
+  		$data = array(
+  		'idmakanan'=>$kode,
+  		'nama' => $nama,
+  		'expire' => $expire,
+  		'halal' => $halal,
+  		'available' => $available,
+  		'ingredients' => $ingredients,
+      'lokasi' => $lokasi
+  		);
+
+  		$result = $this->CRUDModel->InsertUsername($data);
+
+  		$data = NULL;
+  		if($result){
+
+  			redirect('CRUD');
+  		}else{
+
+  			$nama = $this->session->nama;
+  			$data['nama'] = $nama;
+  			$data['result'] = "Gagal";
+  			$this->load->view('InsertMakanan',$data);
+  		}
+  	}
+
 }
