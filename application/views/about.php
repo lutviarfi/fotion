94bf50d0436d32c1ad2222ffd12b6e1996baac04
ansmuf <mufidahanisa@gmail.com>
2018-05-16 14:52:44 +0700

    change profile photo

--- application/views/about.php
+++ application/views/about.php
@@ -1,185 +1,56 @@
 <?php include"application/views/master/header.php" ?>
 <section class="bg-light" id="team">
-		 <div class="container">
-			 <div class="row">
-				 <div class="col-lg-12 text-center">
-					 <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-				 </div>
-			 </div>
-			 <div class="row">
-				 <div class="col-sm-3">
-					 <div class="team-member">
-						 <img class="img-circle" src="<?php blink('assets/img/1.jpg')?>" width="200px"alt="">
-						 <h4>Lutvi Arvianto</h4>
-						 <p class="text-muted">Lead Designer</p>
-						 <ul class="list-inline social-buttons">
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-twitter"></i>
-								 </a>
-							 </li>
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-facebook"></i>
-								 </a>
-							 </li>
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-linkedin"></i>
-								 </a>
-							 </li>
-						 </ul>
-					 </div>
-				 </div>
-				 <div class="col-sm-3">
-					 <div class="team-member">
-						 <img class="img-circle" src="<?php blink('assets/img/2.jpg')?>" width="200px" alt="">
-						 <h4>Larry Parker</h4>
-						 <p class="text-muted">Lead Marketer</p>
-						 <ul class="list-inline social-buttons">
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-twitter"></i>
-								 </a>
-							 </li>
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-facebook"></i>
-								 </a>
-							 </li>
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-linkedin"></i>
-								 </a>
-							 </li>
-						 </ul>
-					 </div>
-				 </div>
-				 <div class="col-sm-3">
-					 <div class="team-member">
-						 <img class="img-circle" src="<?php blink('assets/img/3.jpg')?>" width="200px" alt="">
-						 <h4>Diana Pertersen</h4>
-						 <p class="text-muted">Lead Developer</p>
-						 <ul class="list-inline social-buttons">
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-twitter"></i>
-								 </a>
-							 </li>
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-facebook"></i>
-								 </a>
-							 </li>
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-linkedin"></i>
-								 </a>
-							 </li>
-						 </ul>
-					 </div>
-				 </div>
-				 <div class="col-sm-3">
-					 <div class="team-member">
-						 <img class="img-circle" src="<?php blink('assets/img/3.jpg')?>" width="200px" alt="">
-						 <h4>Diana Pertersen</h4>
-						 <p class="text-muted">Lead Developer</p>
-						 <ul class="list-inline social-buttons">
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-twitter"></i>
-								 </a>
-							 </li>
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-facebook"></i>
-								 </a>
-							 </li>
-							 <li class="list-inline-item">
-								 <a href="#">
-									 <i class="fa fa-linkedin"></i>
-								 </a>
-							 </li>
-						 </ul>
-					 </div>
-				 </div>
-			 </div>
+	<!-- Bootstrap core CSS -->
+ <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-				 <!-- --Baris ke 2-- -->
-				 <div class="row">
-					 <div class="col-sm-3">
-						 <div class="team-member">
-							 <img class="img-circle" src="<?php blink('assets/img/1.jpg')?>" width="200px"alt="">
-							 <h4>Kay Garland</h4>
-							 <p class="text-muted">Lead Designer</p>
-							 <ul class="list-inline social-buttons">
-								 <li class="list-inline-item">
-									 <a href="#">
-										 <i class="fa fa-twitter"></i>
-									 </a>
-								 </li>
-								 <li class="list-inline-item">
-									 <a href="#">
-										 <i class="fa fa-facebook"></i>
-									 </a>
-								 </li>
-								 <li class="list-inline-item">
-									 <a href="#">
-										 <i class="fa fa-linkedin"></i>
-									 </a>
-								 </li>
-							 </ul>
-						 </div>
-					 </div>
-					 <div class="col-sm-3">
-						 <div class="team-member">
-							 <img class="img-circle" src="<?php blink('assets/img/2.jpg')?>" width="200px" alt="">
-							 <h4>Larry Parker</h4>
-							 <p class="text-muted">Lead Marketer</p>
-							 <ul class="list-inline social-buttons">
-								 <li class="list-inline-item">
-									 <a href="#">
-										 <i class="fa fa-twitter"></i>
-									 </a>
-								 </li>
-								 <li class="list-inline-item">
-									 <a href="#">
-										 <i class="fa fa-facebook"></i>
-									 </a>
-								 </li>
-								 <li class="list-inline-item">
-									 <a href="#">
-										 <i class="fa fa-linkedin"></i>
-									 </a>
-								 </li>
-							 </ul>
-						 </div>
-					 </div>
-					 <div class="col-sm-3">
-						 <div class="team-member">
-							 <img class="img-circle" src="<?php blink('assets/img/3.jpg')?>" width="200px" alt="">
-							 <h4>Diana Pertersen</h4>
-							 <p class="text-muted">Lead Developer</p>
-							 <ul class="list-inline social-buttons">
-								 <li class="list-inline-item">
-									 <a href="#">
-										 <i class="fa fa-twitter"></i>
-									 </a>
-								 </li>
-								 <li class="list-inline-item">
-									 <a href="#">
-										 <i class="fa fa-facebook"></i>
-									 </a>
-								 </li>
-								 <li class="list-inline-item">
-									 <a href="#">
-										 <i class="fa fa-linkedin"></i>
-									 </a>
-								 </li>
-							 </ul>
-						 </div>
-					 </div>
-		 </div>
+ <!-- Custom styles for this template -->
+ <link href="css/round-about.css" rel="stylesheet">
+
+<div class="row">
+ <div class="col-lg-12">
+	 <h2 class="text-center">Our Amazing Team</h2>
+ </div>
+ <div class="col-sm-3 text-center mb-3">
+	 <img class="img-circle" src="<?php blink('assets/img/lutfi.jpg') ?>" width="200px" height="200px" alt="">
+	 <h3>Lutvi Arfianto </h3>
+	 <h3><small>Leader</small></h3>
+ </div>
+ <div class="col-sm-3 text-center mb-3">
+	 <img class="img-circle" src="<?php blink('assets/img/iqra.jpg') ?>" width="200px" height="200px" alt="">
+	 <h3>Iqra Habibi</h3>
+	 <h3><small>CO-Leader</small></h3>
+ </div>
+ <div class="col-sm-3 text-center mb-3">
+	 <img class="img-circle" src="<?php blink('assets/img/aldis.jpg') ?>" width="200px" height="200px" alt="">
+	 <h3>Aldis Fakhri S</h3>
+	 <h3><small>Web Developer</small></h3>
+ </div>
+ <div class="col-sm-3 text-center mb-3">
+	 <img class="img-circle" src="<?php blink('assets/img/me.jpg') ?>" width="200px" height="200px" alt="">
+	 <h3>Anisa Mufidah</h3>
+	 <h3><small>Web Developer</small></h3>
+ </div>
+ <div class="col-sm-3 text-center mb-3 off">
+	 <img class="img-circle" src="<?php blink('assets/img/fadila.jpg') ?>" width="200px" height="200px" alt="">
+	 <h3>Fadila Hasana</h3>
+	 <h3><small>Web Developer</small></h3>
+ </div>
+ <div class="col-sm-3 text-center mb-3">
+	 <img class="img-circle" src="<?php blink('assets/img/andy.jpg') ?>" width="200px" height="200px" alt="">
+	 <h3>Andy Chahyono</h3>
+	 <h3><small>Web Developer</small></h3>
+ </div>
+ <div class="col-sm-3 text-center mb-3">
+	<img class="img-circle" src="<?php blink('assets/img/1.jpg') ?>" width="200px" height="200px" alt="">
+	<h3>Kristanto Nugroho</h3>
+	<h3><small>Web Developer</small></h3>
+ </div>
+</div>
+
+<script src="vendor/jquery/jquery.min.js"></script>
+	 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 	 </section>
+
+
 
 <?php include"application/views/master/footer.php" ?>
