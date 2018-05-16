9638dc99101febe00a4ab4777b1ad61d3109ddd5
kristanto <alazerozone@gmail.com>
2018-05-16 13:28:19 +0700

    delete 2 follow us

--- application/views/RegisterResto.php
+++ application/views/RegisterResto.php
@@ -140,19 +140,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					</ul>
 						<span>There are many variations of passages</span>
 				</div>
-				<div class="col-md-4 footer-bottom  animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
-					<h2>Follow Us</h2>
-					<label><i class="glyphicon glyphicon-menu-up"></i></label>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.</p>
-					<ul class="social-ic">
-						<li><a href="#"><i></i></a></li>
-						<li><a href="#"><i class="ic"></i></a></li>
-						<li><a href="#"><i class="ic1"></i></a></li>
-						<li><a href="#"><i class="ic2"></i></a></li>
-						<li><a href="#"><i class="ic3"></i></a></li>
-					</ul>
-
-				</div>
+				
 			<div class="clearfix"> </div>
 
 			</div>
